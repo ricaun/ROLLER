e6770a3d05ba1da65957a7604c560f6d83f2877c
Zizin13 <162662805+Zizin13@users.noreply.github.com>
2025-05-07 14:20:48 -0700

    filling out some engines.c data

---
+++ PROJECTS/ROLLER/engines.h
@@ -0,0 +1,44 @@
+#ifndef _ROLLER_ENGINES_H
+#define _ROLLER_ENGINES_H
+//-------------------------------------------------------------------------------------------------
+#include "Types.h"
+//-------------------------------------------------------------------------------------------------
+
+extern tRev AUTO_A_revs[];
+extern float AUTO_A_spds[];
+extern int AUTO_A_chgs[];
+
+extern tRev DESILVA_revs[];
+extern float DESILVA_spds[];
+extern int DESILVA_chgs[];
+
+extern tRev PULSE_E_revs[];
+extern float PULSE_E_spds[];
+extern int PULSE_E_chgs[];
+
+extern tRev GLOBAL_revs[];
+extern float GLOBAL_spds[];
+extern int GLOBAL_chgs[];
+
+extern tRev MILLION_revs[];
+extern float MILLION_spds[];
+extern int MILLION_chgs[];
+
+extern tRev MISSION_revs[];
+extern float MISSION_spds[];
+extern int MISSION_chgs[];
+
+extern tRev ZIZIN_revs[];
+extern float ZIZIN_spds[];
+extern int ZIZIN_chgs[];
+
+extern tRev REISE_W_revs[];
+extern float REISE_W_spds[];
+extern int REISE_W_chgs[];
+
+extern tRev fast_revs[];
+extern float fast_spds[];
+extern int fast_chgs[];
+
+//-------------------------------------------------------------------------------------------------
+#endif
