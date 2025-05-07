a996e62318c4f2983bd7154f929040743e9d176b
Zizin13 <162662805+Zizin13@users.noreply.github.com>
2025-05-06 20:12:58 -0700

    moving things around to more accurately mirror original file structure

---
+++ PROJECTS/ROLLER/carplans.h
@@ -0,0 +1,18 @@
+#ifndef _ROLLER_CARPLANS
+#define _ROLLER_CARPLANS
+//-------------------------------------------------------------------------------------------------
+#include "Types.h"
+//-------------------------------------------------------------------------------------------------
+
+extern tPolygon xauto_pols[];
+extern tVec3 xauto_coords[];
+extern unsigned int xauto_backs[];
+extern int xauto_places[];
+extern tAnimation xauto_anms[];
+
+//-------------------------------------------------------------------------------------------------
+
+extern tCarDesign CarDesigns[];
+
+//-------------------------------------------------------------------------------------------------
+#endif
