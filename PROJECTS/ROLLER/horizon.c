d34e026ef55551ba0ab643eb48f483cad93e25e1
Zizin13 <162662805+Zizin13@users.noreply.github.com>
2025-08-13 20:02:01 -0700

    fixing __CFSHL__ math

--- PROJECTS/ROLLER/horizon.c
+++ PROJECTS/ROLLER/horizon.c
@@ -310,12 +310,12 @@ void initclouds()
     fRadius = 1800000.0;                        // Start with maximum radius for placement attempt
     do {
       iRandVal1 = rand();                       // Generate random angle1 (theta) for spherical coordinates
-      iAngle1Calc = 3400 * ((iRandVal1 * iRandVal1) % 32768) / 32768;
+      iAngle1Calc = 3400 * (((iRandVal1 * iRandVal1) & 0x7FFF) >> 15);
       //iAngle1Calc = 3400 * ((iRandVal1 * iRandVal1 - (__CFSHL__((iRandVal1 * iRandVal1) >> 31, 15) + ((iRandVal1 * iRandVal1) >> 31 << 15))) >> 15);
-      iAngle1 = (iAngle1Calc % 32768) / 32768 + 520;
+      iAngle1 = ((iAngle1Calc & 0x8000) >> 15) + 520;  // Will be either 520 or 521
       //iAngle1 = ((iAngle1Calc - (__CFSHL__(iAngle1Calc >> 31, 15) + (iAngle1Calc >> 31 << 15))) >> 15) + 520;
       iRandVal2 = rand();                       // Generate random angle2 (phi) for spherical coordinates
-      iAngle2 = iRandVal2 / 2;
+      iAngle2 = (iRandVal2 >> 1) & 1;  // Will be 0 or 1
       //iAngle2 = ((iRandVal2 << 14) - (__CFSHL__(iRandVal2 << 14 >> 31, 15) + (iRandVal2 << 14 >> 31 << 15))) >> 15;
       fCos1Cos2 = tcos[iAngle2] * tcos[iAngle1];// Calculate rotation matrix elements using trigonometric tables
       fSin1Cos2 = tsin[iAngle2] * tcos[iAngle1];
@@ -365,7 +365,7 @@ void initclouds()
       cloud[iCloudIdx].world.fY = 0.0f * fCos1 + fBaseY + 0.0f * fRotComp2;// World Y coordinate: final cloud position after transformation
       cloud[iCloudIdx].world.fZ = fBaseZ + 0.0f * fCos2;// World Z coordinate: final cloud position after transformation
       iRandColorBase = rand();                  // Generate random cloud color/texture index
-      iColorIndex = (5 * iRandColorBase) % 32768 / 32768;
+      iColorIndex = 0; //TODO look at this more
       //iColorIndex = (5 * iRandColorBase - (__CFSHL__((5 * iRandColorBase) >> 31, 15) + ((5 * iRandColorBase) >> 31 << 15))) >> 15;
       cloud[iCloudIdx].iSurfaceType = iColorIndex;
       cloud[iCloudIdx].iSurfaceType = iColorIndex + 0x508;
