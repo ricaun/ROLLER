6d57c4e9c3f705fa45527a423363abb512b62b4e
Zizin13 <162662805+Zizin13@users.noreply.github.com>
2025-09-08 19:58:17 -0700

    adding sosdata.h and _lpSOSSampleList

---
+++ DONS/SOSDIGIW/SOSLIB/SRC/sosdata.h
@@ -0,0 +1,23 @@
+#ifndef _ROLLER_SOSDATA_H
+#define _ROLLER_SOSDATA_H
+//-------------------------------------------------------------------------------------------------
+#include "../../../../PROJECTS/ROLLER/types.h"
+//-------------------------------------------------------------------------------------------------
+
+#pragma pack(push, 2)
+typedef struct
+{
+  void *pSample;
+
+  //unused, purely to maintain 16-bit alignment
+  uint8 byReserved1;
+  uint8 byReserved2;
+} tSOSSampleEntry;
+#pragma pack(pop)
+
+//-------------------------------------------------------------------------------------------------
+
+extern tSOSSampleEntry _lpSOSSampleList[5][32];
+
+//-------------------------------------------------------------------------------------------------
+#endif
